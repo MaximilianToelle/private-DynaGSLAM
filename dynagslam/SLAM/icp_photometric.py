c58034e0c7a8101881bbb6009c377b15f4002f8a
Maximilian Tölle <maximilian.toelle@dfki.de>
2026-07-03 15:39:07 +0200

    further python package import changes

--- dynagslam/SLAM/icp_photometric.py
+++ dynagslam/SLAM/icp_photometric.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from SLAM.utils import *
+from dynagslam.SLAM.utils import *
 import matplotlib.pyplot as plt
 import open3d as o3d
 
